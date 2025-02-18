dd7979e238e0b425ed17473cfc48a89eecd98409
syl <sunyilin@haitutech.com>
2025-02-18 15:30:05 +0800

    测试程序代码

---
+++ PATH_Build/Demo_Sample.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "sample_comm.h"
+//#include "sample_ipc.h"
+#include "securec.h"
+#include "ss_mpi_ae.h"
+
+
+
+#define VI_DEV_ID 0
+#define VI_CHN_ID 0
+#define VO_DEV_ID 0
+#define VO_CHN_ID 0
+
+int main(void)
+{
+
+
+	return 0;
+}
